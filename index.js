21d28d03ed4e73f0eeb03a5ff1afd02606005a4e
Stian Toften <stiantoften@gmail.com>
2023-12-16 14:31:08 +0100

    Adds file load functionality

---
+++ index.js
@@ -0,0 +1,31 @@
+const saveInput = document.getElementById('save-input');
+const editorArea = document.getElementById('editor');
+
+// https://stackoverflow.com/questions/40031688/javascript-arraybuffer-to-hex
+function buf2hex(buffer) { // buffer is an ArrayBuffer
+    return [...buffer]
+        .slice(0, 0x100)
+        .map(x => x.toString(16).padStart(2, '0'))
+        .join(' ');
+}
+
+const readFile = (file) => new Promise((resolve, reject) => {
+    var reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onload = (e) => {
+        resolve(e.target.result);
+    }
+});
+
+saveInput.addEventListener('change', async () => {
+    console.log('changed')
+    const file = saveInput.files[0];
+    if (!file) return;
+
+    const ab = await readFile(file);
+    const arr = new Uint8Array(ab);
+
+    editor.innerHTML += `<br><div class=hex>${buf2hex(arr)}</div>`
+    editor.innerHTML += `<br><div>Bolt count: ${arr[0x24]}</div>`
+}, false);
+
