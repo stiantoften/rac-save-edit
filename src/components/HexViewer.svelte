806726fc7d2b8f24d51ec1b3b3a89c38fc62afbe
Stian Toften <stiantoften@gmail.com>
2023-12-19 16:35:59 +0100

    HexViewer: Add one more character to offset display

--- src/components/HexViewer.svelte
+++ src/components/HexViewer.svelte
@@ -7,7 +7,7 @@
         .map((c, i) => {
             let hexByte = "";
             if (i == 0) {
-                hexByte += `${i.toString(16).padStart(4, "0")}  `;
+                hexByte += `${i.toString(16).padStart(5, "0")}  `;
             }
             if (c) {
                 hexByte += c.toString(16).padStart(2, "0");
@@ -16,7 +16,7 @@
             }
             hexByte += " ";
             if ((i + 1) % 0x10 === 0) {
-                hexByte += `\n${(i + 1).toString(16).padStart(4, "0")}  `;
+                hexByte += `\n${(i + 1).toString(16).padStart(5, "0")}  `;
             } else if ((i + 1) % 0x08 === 0) {
                 hexByte += " ";
             }
