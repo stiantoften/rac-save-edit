7dca0e6bcbf8af61803d03f7d0ae7142c117bf8b
Stian Toften <stiantoften@gmail.com>
2023-12-19 16:10:09 +0100

    HexViewer: Adds offset display

--- src/components/HexViewer.svelte
+++ src/components/HexViewer.svelte
@@ -5,15 +5,18 @@
 <div>
     {[...data]
         .map((c, i) => {
-            let hexByte;
+            let hexByte = "";
+            if (i == 0) {
+                hexByte += `${i.toString(16).padStart(4, "0")}  `;
+            }
             if (c) {
-                hexByte = c.toString(16).padStart(2, "0");
+                hexByte += c.toString(16).padStart(2, "0");
             } else {
-                hexByte = "00";
+                hexByte += "00";
             }
             hexByte += " ";
             if ((i + 1) % 0x10 === 0) {
-                hexByte += "\n";
+                hexByte += `\n${(i + 1).toString(16).padStart(4, "0")}  `;
             } else if ((i + 1) % 0x08 === 0) {
                 hexByte += " ";
             }
