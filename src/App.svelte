bcae932b2063fc8118d8aac40360b18a6357ea1b
Stian Toften <stiantoften@gmail.com>
2023-12-20 23:17:09 +0100

    Adds save functinality

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,7 @@
   import SaveSelector from "./components/SaveSelector.svelte";
   import GameSelector from "./components/GameSelector.svelte";
   import { readUint16 } from "./lib/buffer-reader";
+  import SaveButton from "./components/SaveButton.svelte";
 
   let gameDB = [];
   onMount(async () => {
@@ -71,17 +72,35 @@
     save = null;
   };
 
+  const handleSave = async () => {
+    const handle = await showSaveFilePicker({
+      suggestedName: "save0.bin",
+      types: [
+        {
+          description: "Binary file (.bin)",
+          accept: { "binary/octet-stream": [".bin"] },
+        },
+      ],
+    });
+    const ws = await handle.createWritable();
+    await ws.write(save);
+    await ws.close();
+  };
+
   $: processFile(file);
   $: handleGameChange(selectedGame);
   $: save = selectedSave?.content;
 </script>
 
 <h1>Rachet & Clank Save Editor</h1>
-<div>
+<div class="main">
   {#if !file}
     <Dropzone bind:file />
   {:else}
-    <ResetButton on:click={handleReset} />
+    <div class="buttonbox">
+      <ResetButton on:click={handleReset} />
+      <SaveButton on:click={handleSave} />
+    </div>
     <GameSelector bind:value={selectedGame} {mc} {gameDB} />
     <SaveSelector bind:value={selectedSave} game={selectedGame} />
     {#if save}
@@ -92,11 +111,15 @@
 </div>
 
 <style>
-  div {
+  .main {
     margin: 0 auto;
     max-width: 800px;
     display: flex;
     flex-direction: column;
+    gap: 10px;
+  }
+  .buttonbox {
+    display: flex;
     gap: 10px;
   }
 </style>
--- src/components/SaveButton.svelte
+++ src/components/SaveButton.svelte
@@ -1,10 +1,10 @@
-<button on:click>Reset</button>
+<button on:click>Save</button>
 
 <style>
     button {
         padding: 8px 16px;
         border-radius: 4px;
-        background-color: rgb(255, 104, 104);
+        background-color: rgb(180, 253, 146);
         font-weight: 600;
         outline: none;
         border: none;
